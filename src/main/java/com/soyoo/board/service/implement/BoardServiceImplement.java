da6bd8b268c69bb9a3437fdf29e802e33c98943d
soyoo <soyoo0819@naver.com>
2023-04-26 09:24:25 +0900

    게시판서비스 구현체

---
+++ src/main/java/com/soyoo/board/service/implement/BoardServiceImplement.java
@@ -0,0 +1,10 @@
+package com.soyoo.board.service.implement;
+
+import org.springframework.stereotype.Service;
+
+import com.soyoo.board.service.BoardService;
+
+@Service
+public class BoardServiceImplement implements BoardService {
+    
+}
