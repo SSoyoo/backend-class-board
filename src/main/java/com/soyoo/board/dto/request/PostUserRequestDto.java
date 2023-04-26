8e024c7852ade58065af9d35fc0a3dddccef4bac
soyoo <soyoo0819@naver.com>
2023-04-26 09:51:08 +0900

    회원가입RequestDto생성

---
+++ src/main/java/com/soyoo/board/dto/request/PostUserRequestDto.java
@@ -0,0 +1,10 @@
+package com.soyoo.board.dto.request;
+
+public class PostUserRequestDto {
+    private String userEmail;
+    private String userPassword;
+    private String userNickname;
+    private String userPhoneNumber;
+    private String userAddress;
+    private String userProfileImageUrl;
+}
