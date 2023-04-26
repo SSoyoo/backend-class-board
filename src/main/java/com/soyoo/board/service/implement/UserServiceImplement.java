0f08b8fa8a734c955fbf16fa23f019710eeba0ea
soyoo <soyoo0819@naver.com>
2023-04-26 09:24:16 +0900

    유저서비스 구현체

---
+++ src/main/java/com/soyoo/board/service/implement/UserServiceImplement.java
@@ -0,0 +1,9 @@
+package com.soyoo.board.service.implement;
+
+import org.springframework.stereotype.Service;
+import com.soyoo.board.service.UserService;
+
+@Service
+public class UserServiceImplement implements UserService {
+    
+}
