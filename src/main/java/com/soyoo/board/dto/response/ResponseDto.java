4e4cd5fc4d6c7547ffcb45fb945f1b3a5fdbe624
soyoo <soyoo0819@naver.com>
2023-04-26 10:50:36 +0900

    ResponseDto 응답형식 작성

---
+++ src/main/java/com/soyoo/board/dto/response/ResponseDto.java
@@ -0,0 +1,15 @@
+package com.soyoo.board.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDto {
+
+    private String code;
+    private String message;
+
+}
