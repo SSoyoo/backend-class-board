e862ff92226914be0f5ae3fba96dba371d86ae8f
soyoo <soyoo0819@naver.com>
2023-05-04 10:10:51 +0900

    CustomExceptionHandler return 수정

--- src/main/java/com/soyoo/board/exception/CustomExceptionHandler.java
+++ src/main/java/com/soyoo/board/exception/CustomExceptionHandler.java
@@ -1,19 +1,26 @@
 package com.soyoo.board.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.soyoo.board.common.util.CustomResponse;
 import com.soyoo.board.dto.response.ResponseDto;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ResponseDto> handlerHttpMessageNotReadableException (HttpMessageNotReadableException exception){
-        ResponseDto responseBody 
-        = new ResponseDto("VF", "Request Parameter Validation Failed");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+        return CustomResponse.validationFaild();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDto> handlerMethodArgumentNotValidException(
+        MethodArgumentNotValidException exception) {
+            return CustomResponse.validationFaild();
+    }
+
+
 }
